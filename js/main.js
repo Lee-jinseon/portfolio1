fd9ddc17712ce86192af40c8e8ee47e717aa7f32
Lee-jinseon <96230342+Lee-jinseon@users.noreply.github.com>
2022-02-02 16:58:29 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slideInner').slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    speed:600,
+    prevArrow:'<button class="slick-arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow:'<button class="slick-arrow slick-next"><i class="fas fa-angle-right"></i></button>',
+    fade:false,
+    slidesToShow:2,
+    slidesToScroll:1,
+    centerMode:true,    
+})
